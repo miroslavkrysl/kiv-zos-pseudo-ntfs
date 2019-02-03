0790329d0f8e40d3377419e3fce154657f111c20
Miroslav Krýsl <wilyxjam@gmail.com>
2019-02-03 14:29:09 +0100

    document exceptions in NtfsPartition

--- NtfsPartition.h
+++ NtfsPartition.h
@@ -39,12 +39,15 @@ public:
      * Read the mft item on the given index from the partition.
      *
      * @param index The index of the mft item to be read.
+     *
+     * @throws PartitionMftOutOfBoundsException When the mft item index is out of bounds.
      */
     MftItem ReadMftItem(int32_t index);
 
     /**
      * Read all mft items with the given uid from the partition
      * and sort them by their order.
+     * It calls the ReadMftItem function in the loop.
      *
      * @param items The vector of MftItems to be read.
      */
@@ -54,11 +57,14 @@ public:
      * Write the mft item into its position on the partition.
      *
      * @param item The MftItem to be written.
+     *
+     * @throws PartitionMftOutOfBoundsException When the mft item index is out of bounds.
      */
     void WriteMftItem(MftItem &item);
 
     /**
      * Write the given mft items into their position on the partition.
+     * It calls the WriteMftItem function in the loop.
      *
      * @param items The vector of MftItems to be written.
      */
@@ -68,6 +74,9 @@ public:
      * Read the value of the bitmap bit on the given index.
      *
      * @param index The index of the bit in the bitmap.
+     *
+     * @throws PartitionBitmapOutOfBoundsException When the bit index is out of bounds.
+     *
      * @return The value of the bit on the given index.
      */
     bool ReadBitmapBit(int32_t index);
@@ -76,7 +85,9 @@ public:
      * Write the bitmap bit on the given index.
      *
      * @param index The index of the bit in the bitmap.
-     * @return The value of the bit to be written.
+     * @param bit The value of the bit to be written.
+     *
+     * @throws PartitionBitmapOutOfBoundsException When the bit index is out of bounds.
      */
     void WriteBitmapBit(int32_t index, bool bit);
 
@@ -86,6 +97,9 @@ public:
      * @param index The index of the cluster.
      * @param destination The pointer to the data destination.
      * @param dataSize The size of the data in bytes.
+     *
+     * @throws PartitionDataOutOfBoundsException When the cluster index is out of bounds.
+     * @throws PartitionClusterOverflowException When the dataSize is bigger than the cluster capacity.
      */
     void ReadCluster(int32_t index, void *destination, size_t dataSize);
 
@@ -95,24 +109,33 @@ public:
      * @param index The index of the cluster.
      * @param source The pointer to the data source.
      * @param dataSize The size of the data in bytes.
+     *
+     * @throws PartitionDataOutOfBoundsException When the cluster index is out of bounds.
+     * @throws PartitionClusterOverflowException When the dataSize is bigger than the cluster capacity.
      */
     void WriteCluster(int32_t index, void *source, size_t dataSize);
 
     /**
      * Read the data from the clusters into the destination address.
+     * It calls the ReadCluster function in the loop.
      *
      * @param indexes The vector of the cluster indexes.
      * @param destination The pointer to the data destination.
      * @param dataSize The size of the data in bytes.
+     *
+     * @throws PartitionClusterOverflowException When the dataSize is bigger than the clusters capacity.
      */
     void ReadClusters(std::vector<int32_t> indexes, void *destination, size_t dataSize);
 
     /**
      * Write the data from the source address into the clusters.
+     * It calls the WriteCluster function in the loop.
      *
      * @param indexes The vector of the cluster indexes.
      * @param source The pointer to the data source.
      * @param dataSize The size of the data in bytes.
+     *
+     * @throws PartitionClusterOverflowException When the dataSize is bigger than the clusters capacity.
      */
     void WriteClusters(std::vector<int32_t> indexes, void *source, size_t dataSize);
 
@@ -125,66 +148,88 @@ public:
 
     /**
      * Get the partition boot record.
+     *
+     * @throws PartitionFileNotOpenedException When the partition file isn't opened.
      * @return The partition boot record.
      */
     boot_record GetBootRecord();
 
     /**
      * Get the partition signature (creators login name).
+     *
+     * @throws PartitionFileNotOpenedException When the partition file isn't opened.
      * @return The partition signature.
      */
     std::string GetSignature();
 
     /**
      * Get the partition description
+     *
+     * @throws PartitionFileNotOpenedException When the partition file isn't opened.
      * @return The partition description.
      */
     std::string GetDescription();
 
     /**
      * Get the partition mft start address.
+     *
+     * @throws PartitionFileNotOpenedException When the partition file isn't opened.
      * @return The partition mft start address.
      */
     int32_t GetMftStartAddress();
 
     /**
      * Get the partition bitmap start address.
+     *
+     * @throws PartitionFileNotOpenedException When the partition file isn't opened.
      * @return The partition bitmap start address.
      */
     int32_t GetBitmapStartAddress();
 
     /**
      * Get the partition data start address.
+     *
+     * @throws PartitionFileNotOpenedException When the partition file isn't opened.
      * @return The partition data start address.
      */
     int32_t GetDataStartAddress();
 
     /**
      * Get the partition mft item count.
+     *
+     * @throws PartitionFileNotOpenedException When the partition file isn't opened.
      * @return The partition mft item count.
      */
     int32_t GetMftItemCount();
 
     /**
      * Get the partition mft max fragments count.
+     *
+     * @throws PartitionFileNotOpenedException When the partition file isn't opened.
      * @return The partition mft max fragments count.
      */
     int32_t GetMftMaxFragmentsCount();
 
     /**
      * Get the partition cluster count.
+     *
+     * @throws PartitionFileNotOpenedException When the partition file isn't opened.
      * @return The partition cluster count.
      */
     int32_t GetClusterCount();
 
     /**
      * Get the partition cluster size.
+     *
+     * @throws PartitionFileNotOpenedException When the partition file isn't opened.
      * @return The partition cluster size.
      */
     int32_t GetClusterSize();
 
     /**
      * Get the partition size.
+     *
+     * @throws PartitionFileNotOpenedException When the partition file isn't opened.
      * @return The partition size.
      */
     int32_t GetPartitionSize();
@@ -211,6 +256,9 @@ private:
      * @param position The read position.
      * @param destination The pointer to the data destination.
      * @param size The size of the data in bytes.
+     *
+     * @throws PartitionFileNotOpenedException When the partition file isn't opened.
+     * @throws PartitionOutOfBoundsException When the position is out of partition bounds.
      */
     void Read(int32_t position, void *destination, size_t size);
 
@@ -220,6 +268,9 @@ private:
      * @param position The write position.
      * @param source The pointer to the data source.
      * @param size The size of the data in bytes.
+     *
+     * @throws PartitionFileNotOpenedException When the partition file isn't opened.
+     * @throws PartitionOutOfBoundsException When the position is out of partition bounds.
      */
     void Write(int32_t position, void *source, size_t size);
 
@@ -227,7 +278,7 @@ private:
      * Do a basic boot record values validation.
      *
      * @param bootRecord The boot record to be validated.
-     * @return
+     * @return True if the boot record values passed, false otherwise.
      */
     bool ValidateBootRecord(boot_record &bootRecord);
 
